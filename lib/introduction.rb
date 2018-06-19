ab6c27e9fd98f976e310b57b5710e0ed53765c1f
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-19 16:41:31 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
-def #introduction_with_language
+def introduction_with_language
   puts "hi my name is #{name}"
   puts "I am learning to program in #{language}"
 end
-#introduction_with_language
+introduction_with_language
